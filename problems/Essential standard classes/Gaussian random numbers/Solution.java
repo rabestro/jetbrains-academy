ff830b845f07593dcfd50d8d6dcb751efad8e1e3
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-29 14:03:09 +0300

    Refactoring

--- problems/Essential standard classes/Gaussian random numbers/Solution.java
+++ problems/Essential standard classes/Gaussian random numbers/Solution.java
@@ -1,19 +1,22 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.DoubleStream;
 
-import static java.util.stream.IntStream.range;
 import static java.util.stream.LongStream.iterate;
 
-public class Main {
+public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        final var scanner = new Scanner(System.in);
         final var initialSeed = scanner.nextInt();
         final var numbersCount = scanner.nextInt();
         final var maxGaussian = scanner.nextDouble();
 
         iterate(initialSeed, i -> i + 1).filter(seed -> {
             final var random = new Random(seed);
-            return range(0, numbersCount).allMatch(i -> random.nextGaussian() <= maxGaussian);
+            return DoubleStream
+                    .generate(random::nextGaussian)
+                    .limit(numbersCount)
+                    .allMatch(d -> d <= maxGaussian);
         }).findFirst()
                 .ifPresent(System.out::print);
     }
