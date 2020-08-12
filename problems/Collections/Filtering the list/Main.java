7b7e43a8d8c22b0585d1d46416a1564754392c2d
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-08-12 11:27:45 +0300

    Create Main.java

---
+++ problems/Collections/Filtering the list/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        final var scanner = new Scanner(System.in);
+        final var list = new ArrayList<String>();
+
+        for (var isOdd = false; scanner.hasNext(); isOdd ^= true) {
+            final var number = scanner.next();
+            if (isOdd) {
+                list.add(number);
+            }
+        }
+        Collections.reverse(list);
+        System.out.println(String.join(" ", list.toArray(String[]::new)));
+    }
+}
