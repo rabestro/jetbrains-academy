4dfe0051b562d8a0021e17d0341e9b8a567e42b8
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-24 09:26:03 +0300

    Create Counter.java

---
+++ problems/Collections/Count the value occurrences/Counter.java
@@ -0,0 +1,5 @@
+class Counter {
+    public static boolean checkTheSameNumberOfTimes(int elem, List<Integer> list1, List<Integer> list2) {
+        return Collections.frequency(list1, elem) == Collections.frequency(list2, elem);
+    }
+}
